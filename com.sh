91e9dc5b4ec37d1f2cedbceaa19797930392b3b8
verikca <49689442+verikca@users.noreply.github.com>
2022-09-21 00:40:43 -0400

    Update com.sh

--- com.sh
+++ com.sh
@@ -3,5 +3,5 @@
 [ -z "$1" ] && exit
 
 while IFS= read -r line; do
-    mkdir "$1/$line" && dd if=/dev/zero of="$1/$line/index.html" bs=1 count=$(expr $(date +%N) % 1024)
+    mkdir "$1/$line" && dd if=/dev/zero of="$1/$line/index.html" bs=1 count=$(expr $(date +%N) % 10240)
 done < common.txt
