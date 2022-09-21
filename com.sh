29dd28ffb71157d6550e986b0bc062552b7b6984
verikca <49689442+verikca@users.noreply.github.com>
2022-09-21 00:39:32 -0400

    Create com.sh

---
+++ com.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+[ -z "$1" ] && exit
+
+while IFS= read -r line; do
+    mkdir "$1/$line" && dd if=/dev/zero of="$1/$line/index.html" bs=1 count=$(expr $(date +%N) % 1024)
+done < common.txt
